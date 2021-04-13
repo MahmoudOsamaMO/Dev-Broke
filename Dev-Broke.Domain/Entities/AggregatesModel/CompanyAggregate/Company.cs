85b6af2402eec62ffe35786ffbc783a3250a5a97
Mahmoud Osama <Mahmoud.Osama@integrant.com>
2021-04-13 11:15:06 +0200

    init ddd

---
+++ Dev-Broke.Domain/Entities/AggregatesModel/CompanyAggregate/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev_Broke.Domain.Entities.AggregatesModel.CompanyAggregate
+{
+    public class Company
+    {
+        public Address CompanyAddress { get; private set; }
+
+    }
+}
---
+++ Dev-Broke.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev_Broke.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+    }
+}
